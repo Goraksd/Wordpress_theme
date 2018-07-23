69ba588ac40e137bacd6855f0274cc54dd3e8196
Goraksd <gskakunov518@gmail.com>
2018-07-23 13:11:19 +0300

    Theme

---
+++ page.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html5>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About me</title>
+	<link href="<?php bloginfo('template_directory')?>/style.css" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_directory')?>/img/favicov.jpg"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+	<main class="page-wrap">
+	<header>	
+		<div class="head">
+			<div class="name">
+				<br>
+				Жора Скакунов
+			</div>
+			<br>
+			<div class="photo_me">
+				<img align="center" src="<?php bloginfo('template_directory')?>/img\me.jpg"/>
+			</div>
+			<div class="text_me">
+				<p align="center">Мне 16 лет. Учусь в школе, перешел в 11 класс. Люблю игры и научпоп.</p>
+			</div>
+		</div>
+	</header>
+	<article>
+		<div class="artc_games">
+			<div class="games">
+				<div class="game_text">
+					<p align="center">Favourite games</p>
+				</div>
+				<div class="games_row">
+					<div class="game"><div class="gameico" style="background-image: url(<?php bloginfo('template_directory')?>/img/pd2.jpg);"></div></div>
+					<div class="game"><div class="gameico" style="background-image: url(<?php bloginfo('template_directory')?>/img/r3.jpg");></div></div>
+					<div class="game"><div class="gameico" style="background-image: url(<?php bloginfo('template_directory')?>/img/witcher_3.jpg");></div></div>
+					<div class="game"><div class="gameico" style="background-image: url(<?php bloginfo('template_directory')?>/img/dirt_4.jpg");></div></div>
+					<div class="game"><div class="gameico" style="background-image: url(<?php bloginfo('template_directory')?>/img/Titanfall-2_v66x.jpg");></div></div>
+					<div class="game"><div class="gameico" style="background-image: url(<?php bloginfo('template_directory')?>/img/Inquisition_heraldry_DA2.png");></div></div>
+				</div>
+			</div>
+		</div>
+	</article>
+	<article>
+		<div class="video_main">
+			<div class="video_text">
+				<p align="center">Моё школьное творчество</p>
+			</div>
+			 <div class="video">
+				<iframe   src="https://www.youtube.com/embed/FhWtYM-GZxI" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>
+			</div> 
+		</div>
+	</article>
+	<article>
+		<div class="why">
+			<div class="title">
+				<p align="center">Почему я пришел на практику?</p>
+			</div>
+			<div class="why_text">
+				<p>Интернет — это почти 3,5 миллиарда пользователей и свыше миллиарда сайтов. Стремительное развитие информационных технологий привело к появлению профессий, о которых 10 лет назад никто даже не думал. Верхние строчки в рейтингах самых востребованных и высокооплачиваемых IT-специальностей занимает профессия веб-разработчика. И я хочу оказаться в этой сфере. Развиваться и само-усовершенствоваться.</p>
+			</div>
+		</div>
+	</article>
+	</main>
+	<footer>
+			<div class="foot">
+				<div class="social_row">
+					<div class="social"><a href="http://backend3devtm"><div class="social_ico" class="blog"  style="background-image: url(<?php bloginfo('template_directory')?>/img/shield.png);"></div></a></div>
+					<div class="social"><a  href="https://vk.com/matrixsorro"><div class="social_ico" class="vk" style="background-i
+					<div class="social"><a href="https://www.instagram.com/matrixsorro/"><div class="social_ico" class="inst"  style="background-image: url(<?php bloginfo('template_directory')?>/img/instagram.png);"></div></a></div>
+					<div class="social"><a  href="https://vk.com/matrixsorro"><div class="social_ico" class="vk" style="background-image: url(<?php bloginfo('template_directory')?>/img/vkontakte.png");></div></a></div>
+					<div class="social"> <a href="https://github.com/Goraksd"><div class="social_ico" class="git"  style="background-image: url(<?php bloginfo('template_directory')?>/img/Git.png");></div></a></div>
+				</div>
+			</div>
+	</footer>
+
+</body>
+</html>
